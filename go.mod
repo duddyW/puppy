6292b0f27c639067d0b7a4fb728f1515a05d68ed
Kyrylo Dudka <admin@dudbook-pro.local>
2024-06-12 21:20:45 +0300

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/duddyW/puppy
 
 go 1.22.3
+
+require github.com/duddyW/dog v0.0.0-20240612175906-a6ea507648d5 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/duddyW/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBurk() string {
+	return dog.WhenGrowUp(Bark())
+}
+
+func BigBurks() string {
+	return dog.WhenGrowUp(Barks())
 }
